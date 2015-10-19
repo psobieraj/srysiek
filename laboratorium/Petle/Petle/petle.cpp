a90bb37a51565a4a5c6c1156e01612b74288dc76
psobieraj <srysiomana@gmail.com>
2015-10-19 13:28:31 +0200

    Praca z petlami na laborce

---
+++ laboratorium/Petle/Petle/petle.cpp
@@ -0,0 +1,108 @@
+// Patryk Sobieraj Teleinformatyka gr1 sekcja 1
+#include <iostream>
+#include <ctime>
+#include <cmath>
+using namespace std;
+int n, i, licznik, proba;
+int wynik;
+int start, stop, podstawa, wykladnik, dzielnik=0;
+
+int main()
+
+{   
+	// Pierwsze zad
+	/* cout << "Podaj ile wynosi Twoje n \n";
+	cin >> n;
+
+	for (i = 0;i < n;i++)
+		cout << i << endl; */
+
+	// Drugie zad
+
+/*	cout << " Podaj start \n";
+	cin >> start;
+	cout << " Podaj stop \n";
+	cin >> stop;
+	cout << " Podaj dzielnik liczb \n";
+	cin >> dzielnik;
+
+	for (i = start; i < stop; i++ )
+	{
+		if ((i % dzielnik) == 0)
+		{
+			cout << i << endl;
+
+
+		}
+	}
+
+	*/
+	// program 3
+/*
+	cout << " podstawa \n";
+	cin >> podstawa;
+	cout << " wykladnik \n";
+	cin >> wykladnik;
+	int z = podstawa;
+	for (i = 1; i < wykladnik; i++)
+	{
+		podstawa = podstawa * z;
+		cout << podstawa << endl;
+		
+	}
+
+	*/
+
+	//program 4
+
+	/*cout << "Witaj, pomyslalem liczba z przedzialu 1-20 \n";
+	srand(time(NULL));
+	int liczba = rand() % 20 + 1;
+	do {
+		
+		
+	
+		cin >> proba;
+		if (proba < liczba) cout << "podaj wieksza liczbe \n";
+		if (proba > liczba) cout << "podaj mniejsza liczbe \n";
+		
+
+
+
+
+
+
+	} while (proba != liczba); cout << "gratulacje, zgadles \n";
+	 */
+
+	//program 5
+
+	/* cout << "Witaj, pomyslalem liczba z przedzialu 1-20 \n";
+	srand(time(NULL));
+	int liczba = rand() % 20 + 1;
+	cout << " masz 5 prob " << endl;
+	for (int i=0; i<5; i++)
+	{
+		cout << " masz 5 prob " << endl;
+		cin >> proba;
+		if (proba < liczba) cout << "podaj wieksza liczbe \n";
+		if (proba > liczba) cout << "podaj mniejsza liczbe \n";
+
+
+
+
+
+
+
+		if (proba == liczba) cout << "gratulacje, zgadles \n";
+		}
+		*/
+
+	//program 6
+
+		
+	
+	return 0;
+
+	
+}
