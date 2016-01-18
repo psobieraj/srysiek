4de2408a89ff88ca77ccd7726227a181f8904b56
marekkokot <marek.koki@gmail.com>
2016-01-18 21:23:31 +0100

    Created By Teacher

---
+++ Zadanko1/KolokwiumProbne/Algorytmiczne.h
@@ -0,0 +1,10 @@
+#ifndef _ALGORYTMICZNE_H
+#define _ALGORYTMICZNE_H
+
+#define N 3
+#define M 4
+
+void Print(char tab[N][M]);
+void Move(char in[N][M], char out[N][M], int x, int y);
+
+#endif // ifndef _ALGORYTMICZNE_H
